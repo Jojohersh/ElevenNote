98c5cb8da0fd636a742c6fb9a1635cdbb20ff433
jordan <jojohersh@gmail.com>
2022-12-14 19:29:07 -0500

    DI for user service setup

--- ElevenNote.WebAPI/Program.cs
+++ ElevenNote.WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using ElevenNote.Data;
+using ElevenNote.Services.User;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Add User Service/Interface for Dependency Injection here
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
