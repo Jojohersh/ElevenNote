6f67bdd64b48263412cd6f32dada25f4708268ff
jordan <jojohersh@gmail.com>
2022-12-15 18:47:36 -0500

    implemented GetUserById functionality

--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -33,5 +33,18 @@ namespace ElevenNote.WebAPI.Controllers
             }
             return BadRequest("User could not be registered");
         }
+    
+        
+        [HttpGet("{userId:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int userId)
+        {
+            var foundUser = await _service.GetUserByIdAsync(userId);
+            if (foundUser is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(foundUser);
+        }
     }
 }
