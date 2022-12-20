a63e69d9796b161836c1af1958f289b3b3314953
jordan <jojohersh@gmail.com>
2022-12-20 18:21:32 -0500

    added GetAllNotes route functionality

--- ElevenNote.Services/Note/NoteService.cs
+++ ElevenNote.Services/Note/NoteService.cs
@@ -4,13 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using ElevenNote.Models.Note;
+using ElevenNote.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Services.Note
 {
     public class NoteService : INoteService
     {
         private readonly int _userId;
-        public NoteService(IHttpContextAccessor httpContextAccessor)
+        private readonly ApplicationDbContext _dbContext;
+        public NoteService(IHttpContextAccessor httpContextAccessor, ApplicationDbContext dbContext)
         {
             var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
             var value = userClaims.FindFirst("Id")?.Value;
@@ -19,6 +23,23 @@ namespace ElevenNote.Services.Note
             {
                 throw new Exception("Attempted to build NoteService without User Id claim.");
             }
+
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<NoteListItem>> GetAllNotesAsync()
+        {
+            var notes = await _dbContext.Notes
+                .Where(noteEntity => noteEntity.OwnerId == _userId)
+                .Select(noteEntity => new NoteListItem
+                {
+                    Id = noteEntity.Id,
+                    Title = noteEntity.Title,
+                    CreatedUTC = noteEntity.CreationUTC
+                })
+                .ToListAsync();
+            
+            return notes;
         }
     }
 }
